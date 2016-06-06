b58db32ee13c931a7c1b925f8b07f809b47cedff
Sujay Busam <sujay.busam@gmail.com>
2016-06-06 17:26:44 -0400

    Associated values examples

--- CodeSamplesPlayground.playground/Contents.swift
+++ CodeSamplesPlayground.playground/Contents.swift
@@ -10,3 +10,40 @@ enum LoginProvider: String {
 }
 
 let provider = LoginProvider.init(rawValue: "email")
+
+
+// Associated Values
+
+enum LoginProviderAV {
+    case Email(String, String)
+    case Facebook
+    case Twitter
+    case Google
+}
+
+let providerAV = LoginProviderAV.Email("foo@bar.com", "password")
+
+
+
+// Associated Values Continued
+
+struct LoginUser {
+    let email: String
+    let password: String
+    
+    func isValid() -> Bool {
+        // Implement email / password validation
+        return true
+    }
+}
+
+enum LoginProviderAVC {
+    case Email(LoginUser)
+    case Facebook
+    case Twitter
+    case Google
+}
+
+let user = LoginUser(email: "foo@bar.com", password: "password")
+let providerAVC = LoginProviderAVC.Email(user)
+
