121e6f67fecb6953e6713b73fc239ce7dd6a4602
Sujay Busam <sujay.busam@gmail.com>
2016-06-06 16:43:19 -0400

    Raw value example

--- CodeSamplesPlayground.playground/Contents.swift
+++ CodeSamplesPlayground.playground/Contents.swift
@@ -1,2 +1,12 @@
 import UIKit
 
+// Raw Values
+
+enum LoginProvider: String {
+    case Email = "email"
+    case Facebook = "facebook"
+    case Twitter = "twitter"
+    case Google = "google"
+}
+
+let provider = LoginProvider.init(rawValue: "email")
