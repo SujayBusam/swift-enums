1c3f0495314ac3abdafe98aa2dfddd0da1a0d198
Sujay Busam <sujay.busam@gmail.com>
2016-06-07 01:23:08 -0400

    Implement email login

---
+++ SwiftEnums/SwiftEnums/LoginProvider.swift
@@ -0,0 +1,52 @@
+//
+//  LoginProvider.swift
+//  SwiftEnums
+//
+//  Created by Sujay Busam on 6/6/16.
+//  Copyright © 2016 Sujay's Dev Center. All rights reserved.
+//
+
+import Foundation
+
+protocol LoginProviderDelegate {
+    func loginProvider(loginProvider: LoginProvider, didSucceed successMessage: String)
+    func loginProvider(loginProvider: LoginProvider, didFail error: String)
+}
+
+struct LoginUser {
+    let email: String
+    let password: String
+    
+    func isValid() -> Bool {
+        // Simple validation
+        return !email.isEmpty && !password.isEmpty
+    }
+}
+
+enum LoginProvider {
+    case Email(LoginUser)
+    case Facebook
+    case None
+    
+    func login(delegate: LoginProviderDelegate) {
+        switch self {
+        case let .Email(user) where user.isValid():
+            loginWithEmail(user.email, password: user.password, delegate: delegate)
+            break
+        case let .Email(user) where !user.isValid():
+            print("Invalid user")
+            break
+        default:
+            break
+        }
+    }
+    
+    // ============ Helper Functions ============ //
+    
+    private func loginWithEmail(email: String, password: String, delegate: LoginProviderDelegate) {
+        // Mock out success
+        delegate.loginProvider(self, didSucceed: "Email \(email) login success")
+    }
+}
+
+
--- SwiftEnums/SwiftEnums/ViewController.swift
+++ SwiftEnums/SwiftEnums/ViewController.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginProviderDelegate {
     
     // ============ Text Field Instance Variables ============ //
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
+    var loginProvider = LoginProvider.None
+    
 
     // ============ Login Button Actions ============ //
     
     @IBAction func emailLoginTouched(sender: UIButton) {
-        
+        let user = LoginUser(email: emailField.text!, password: passwordField.text!)
+        loginProvider = .Email(user)
+        loginProvider.login(self)
     }
     
     @IBAction func facebookLoginTouched(sender: UIButton) {
         
     }
     
+    
+    // ============ LoginProviderDelegate Methods ============ //
+    
+    func loginProvider(loginProvider: LoginProvider, didSucceed successMessage: String) {
+        print(successMessage)
+    }
+    
+    func loginProvider(loginProvider: LoginProvider, didFail error: String) {
+        
+    }
 
+    
 }
 
